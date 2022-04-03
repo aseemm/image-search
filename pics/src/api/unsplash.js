a706a4e6f23ad9bca3376dec15e9f8be75b72832
Aseem Maheshwari <aseemm@gmail.com>
2022-04-03 15:09:13 -0700

    refactor code

--- pics/src/api/unsplash.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-/* configure a pre-configured client */
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: "Client-ID sJBceKS5BxdFMQnZX5Bozs1Y3Wnq7L9V-Fvu2nNBEkc",
-      },
-});
